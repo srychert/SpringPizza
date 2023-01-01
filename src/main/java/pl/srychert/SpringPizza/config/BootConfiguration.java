deffc027497244c2752b532a87ee83ea246cb20b
Sebastian Rychert <sebastian_rychert@wp.pl>
2023-01-01 21:19:07 +0100

    add better objectMapper

--- src/main/java/pl/srychert/SpringPizza/config/BootConfiguration.java
+++ src/main/java/pl/srychert/SpringPizza/config/BootConfiguration.java
@@ -1,5 +1,7 @@
 package pl.srychert.SpringPizza.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -13,5 +15,12 @@ public class BootConfiguration {
     @Primary
     PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new Hibernate5JakartaModule());
+        return objectMapper;
     }
 }
