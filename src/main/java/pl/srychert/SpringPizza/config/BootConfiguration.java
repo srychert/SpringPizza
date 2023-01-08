cbd48cbb9d927c45ecad98a539706daaccc585e8
Sebastian Rychert <sebastian_rychert@wp.pl>
2023-01-08 14:14:57 +0100

    set locale to english

--- src/main/java/pl/srychert/SpringPizza/config/BootConfiguration.java
+++ src/main/java/pl/srychert/SpringPizza/config/BootConfiguration.java
@@ -7,6 +7,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class BootConfiguration {
@@ -22,5 +26,12 @@ public class BootConfiguration {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new Hibernate5JakartaModule());
         return objectMapper;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.ENGLISH);
+        return slr;
     }
 }
